e546d8f59fdfddd868ec4e83d7cdceb50adacd3d
VictorDolsan <victorddf5@gmail.com>
2023-03-17 21:18:24 -0300

    Inserido pesquisa por url

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-const links = document.querySelectorAll('.header-menu a');
+// Ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
-    const url = location.href;
-    const linkHref = link.href;
+function ativarLink(link) {
+  const url = location.href;
+  const linkHref = link.href;
 
-    if (url.includes(linkHref)){
-        link.classList.add('ativo');
-    }else {
-        link.classList.remove('ativo');
-    }
+  if (url.includes(linkHref)) {
+    link.classList.add("ativo");
+  } else {
+    link.classList.remove("ativo");
+  }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
