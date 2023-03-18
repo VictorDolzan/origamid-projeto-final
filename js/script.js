a4116032c311a505e0ad06ea1c6335521db7da5e
VictorDolsan <victorddf5@gmail.com>
2023-03-18 11:19:26 -0300

    Inserindo plugin externo

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-  const url = location.href;
-  const linkHref = link.href;
+    const url = location.href;
+    const linkHref = link.href;
 
-  if (url.includes(linkHref)) {
-    link.classList.add("ativo");
-  } else {
-    link.classList.remove("ativo");
-  }
+    if (url.includes(linkHref)) {
+        link.classList.add("ativo");
+    } else {
+        link.classList.remove("ativo");
+    }
 }
 
 links.forEach(ativarLink);
@@ -20,10 +20,10 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-  const elemento = document.getElementById(parametro);
-  if (elemento) {
-    elemento.checked = true;
-  }
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
 }
 
 parametros.forEach(ativarProduto);
@@ -32,17 +32,17 @@ parametros.forEach(ativarProduto);
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
-  const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute("aria-controls");
-  const resposta = document.getElementById(controls);
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
 
-  resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa");
-  pergunta.setAttribute("aria-expanded", ativa);
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener("click", ativarPergunta);
+    pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
@@ -53,13 +53,13 @@ const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
 function trocarImagem(event) {
-  const img = event.currentTarget;
-  const media = matchMedia("(min-width: 1000px)").matches;
-  if (media) {
-    galeriaContainer.prepend(img);
-  }
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
 }
 
 galeria.forEach((img) => {
-  img.addEventListener("click", trocarImagem);
+    img.addEventListener("click", trocarImagem);
 });
